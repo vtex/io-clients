8788766d94431722b86facbb7b9ea68ce1611b41
gabrielHosino <gabriel.hosino@vtex.com.br>
2021-07-06 09:52:02 -0300

    Add listInventoryBySku to logistics client

--- src/typings/logistics.ts
+++ src/typings/logistics.ts
@@ -51,3 +51,16 @@ export interface NearPickupPointsArgs {
 export interface PickupPointArgs {
   id: string
 }
+
+export interface StockInfo {
+  warehouseId: string
+  warehouseName: string
+  totalQuantity: number
+  reservedQuantity: number
+  hasUnlimitedQuantity: number
+}
+
+export interface InventoryBySkuResponse {
+  skuId: string
+  balance: [StockInfo]
+}
